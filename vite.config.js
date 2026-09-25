960a60398274463aa5d53fdd0ee70f05a76dc9a9
Carl Magno <https://github.com/magnocarltorres-jpg>
2026-09-25 18:44:07 +0800

    Fix Cloudflare asset paths

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-    base: "/UDM-Health-Care-Monitoring-System/",
+    base: "/",
     plugins: [
         react(),
         tailwindcss(),
     ],
 })
-
